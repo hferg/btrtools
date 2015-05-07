41cfb3be451be48ec4aa0f485d44a3ab275f0199
hferg <h.ferguson.gow@gmail.com>
2015-05-07 10:00:26 +0100

    added viewlogfile.R for checking output of mcmc

--- R/viewLogfile.R
+++ R/viewLogfile.R
@@ -2,9 +2,15 @@
 #'
 #' A simple function that shows the logfile.
 #' @param logfile The name of the logfile.
+#' @param n 
 #' @export
 
-viewLogfile <- function(logfile) {
-  return(btmcmc(logfile))
+viewLogfile <- function(logfile, n = "max") {
+  if (n == "max") {
+    out <- btmcmc(logfile)
+  } else {
+    out <- head(btmcmc(logfile), n)
   }
+  return(out)
+}
 
