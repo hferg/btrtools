810d9b51e794e7a63f856533e6b0ac1fadfbe6f2
hferg <h.ferguson.gow@gmail.com>
2015-05-07 10:00:21 +0100

    added viewlogfile.R for checking output of mcmc

---
+++ R/viewLogfile.R
@@ -0,0 +1,10 @@
+#' viewLogfile
+#'
+#' A simple function that shows the logfile.
+#' @param logfile The name of the logfile.
+#' @export
+
+viewLogfile <- function(logfile) {
+  return(btmcmc(logfile))
+  }
+
