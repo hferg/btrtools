d11234916d656542a1f1a3858825257f39fc24c8
hferg <h.ferguson.gow@gmail.com>
2015-08-03 16:00:26 +0100

    added a regression line to the means plot

--- R/ggRunmean.R
+++ R/ggRunmean.R
@@ -3,12 +3,24 @@
 #' Make a running mean plot for an MCMC parameter using ggplot2.
 #' @param dat The vector of the paramater you want to see an autocorrelation plot for.
 
-ggRunmean <- function(output, pars, title = ""){
-  runmeans <- cumsum(output[ ,pars])/seq(along = output[, pars])
-  z <- ggplot(data.frame(Iteration = c(1:length(runmeans)), runningmean = runmeans), aes(x = Iteration, y = runningmean)) +
+ggRunmean <- function(output, pars, window.size = 10, title = "") {
+  dat <- output[ , pars]
+  windows <- seq.int(window.size, length(dat), window.size)
+  
+  if (windows[length(windows)] != length(dat)) {
+    windows <- c(windows, length(dat))
+  }
+
+  means <- vector(mode = "numeric", length = length(windows))
+
+  for (i in 1:length(windows)) {
+    means[i] <- mean(dat[c((windows[i] - (window.size -1)):windows[i])])
+  }
+
+  z <- ggplot(data.frame(Window = c(1:length(windows)), mean = means), aes(x = Window, y = mean)) +
     geom_line(color = "dodgerblue") +
-    ggtitle(paste(pars, title))
- 
+    ggtitle(paste(pars, title)) +
+    geom_smooth(method = "lm", formula = y ~ x)
   return(z)
 }
 
