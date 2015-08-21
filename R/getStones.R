74f084607bd231e1d8cba18d230608e66bc3eb36
hferg <h.ferguson.gow@gmail.com>
2015-08-21 10:37:00 +0100

    Added a function that makes a table to compare marginal likelihoods froms tepping stone samplers. Called getStones

---
+++ R/getStones.R
@@ -0,0 +1,10 @@
+#' getStones
+#'
+#' Get the marginal likelihoods from a single, or vector of, stepping stones log files.
+#' @param logs A vector of one or more log files from stepping stones analysis.
+#' @name getStones
+#' @export
+
+getStones <- function(logs) {
+  
+}
